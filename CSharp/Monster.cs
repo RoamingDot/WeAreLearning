00837aadd9ec5e62a653783aef6af3653948f655
RoamingDot <mana_ryuu@hotmail.com>
2015-06-04 21:41:41 -0600

    asdf
    asf

---
+++ CSharp/Monster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeeEnDee
+{
+    class Monster : Entity
+    {
+        public string type {get; set;}
+        public int challenge { get; set; }
+        public string appearance { get; set; }
+        public string actions { get; set; }
+        public string page { get; private set; }
+
+        public override string ToString()
+        {
+            return "Entity: " + Name + '(' + Race + ')';
+        }
+    }
+}
